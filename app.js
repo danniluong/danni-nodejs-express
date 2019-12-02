356f1e5dd148af94313120ef71bacd7873ad7920
dannil@dk.ibm.com <danniluong@gmail.com>
2019-12-02 12:54:51 +0100

    new1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody, Danni Luong!");
+  res.send("Hello from Appsody, Danni Luong 20191201!");
 });
  
 module.exports.app = app;
